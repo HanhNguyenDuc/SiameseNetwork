5c841ba21bce7f5ac45a0b5f65119c1a9f32a6a5
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-07-25 16:39:32 +0700

    add dilation_rate

--- dataset_cifar10.py
+++ dataset_cifar10.py
@@ -5,6 +5,8 @@ from keras.models import Model
 import numpy as np
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
+# from scipy.utils import Shuffle
+
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
@@ -13,35 +15,49 @@ from keras.preprocessing.image import ImageDataGenerator
 X_train = X_train / 255
 X_test = X_test / 255
 
+endp = int(X_train.shape[0] * 0.9)
+X_val = X_train[endp:]
+y_val = y_train[endp:]
+X_train = X_train[:endp]
+y_train = y_train[:endp]
+
 IMG_SHAPE = X_train.shape[1:]
+
+datagen = ImageDataGenerator(
+          rotation_range = 10,
+          width_shift_range = 0.1,
+          height_shift_range = 0.1, 
+          horizontal_flip = True
+)
 
 
 def SiameseNetworkModel():
   input_ = Input(shape = IMG_SHAPE)
   
   #Block conv 1
-  conv_1_32 = Conv2D(32, kernel_size = (3, 3), padding = 'same')(input_)
+  conv_1_32 = Conv2D(32, kernel_size = (3, 3), padding = 'same', dilation_rate = (1, 1))(input_)
   drop_1_32 = Dropout(0.25)(conv_1_32)
   maxpool_1_32 = MaxPooling2D(pool_size = (2, 2))(drop_1_32)
   
-  conv_1_64 = Conv2D(64, kernel_size = (3, 3), padding = 'same')(maxpool_1_32)
+  
+  conv_1_64 = Conv2D(64, kernel_size = (3, 3), padding = 'same', dilation_rate = (1, 1))(maxpool_1_32)
   drop_1_64 = Dropout(0.25)(conv_1_64)
   maxpool_1_64 = MaxPooling2D(pool_size = (2, 2))(drop_1_64)
   
-  conv_1_128 = Conv2D(128, kernel_size = (3, 3), padding = 'same')(maxpool_1_64)
+  conv_1_128 = Conv2D(128, kernel_size = (3, 3), padding = 'same', dilation_rate = (1, 1))(maxpool_1_64)
   drop_1_128 = Dropout(0.25)(conv_1_128)
   maxpool_1_128 = MaxPooling2D(pool_size = (2, 2))(drop_1_128)
   
   #Block conv 2
-  conv_2_32 = Conv2D(32, kernel_size = (3, 3), padding = 'same')(input_)
+  conv_2_32 = Conv2D(32, kernel_size = (3, 3), padding = 'same', dilation_rate = (2, 2))(input_)
   drop_2_32 = Dropout(0.25)(conv_2_32)
   maxpool_2_32 = MaxPooling2D(pool_size = (2, 2))(drop_2_32)
   
-  conv_2_64 = Conv2D(64, kernel_size = (3, 3), padding = 'same')(maxpool_2_32)
+  conv_2_64 = Conv2D(64, kernel_size = (3, 3), padding = 'same', dilation_rate = (2, 2))(maxpool_2_32)
   drop_2_64 = Dropout(0.25)(conv_2_64)
   maxpool_2_64 = MaxPooling2D(pool_size = (2, 2))(drop_2_64)
   
-  conv_2_128 = Conv2D(128, kernel_size = (3, 3), padding = 'same')(maxpool_2_64)
+  conv_2_128 = Conv2D(128, kernel_size = (3, 3), padding = 'same', dilation_rate = (2, 2))(maxpool_2_64)
   drop_2_128 = Dropout(0.25)(conv_2_128)
   maxpool_2_128 = MaxPooling2D(pool_size = (2, 2))(drop_2_128)
 
@@ -60,14 +76,15 @@ checkpoint = ModelCheckpoint('weight.hdf5', monitor = 'val_acc', save_best_only 
 
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
-model.fit(X_train, y_train, epochs = 25, validation_split = 0.1, callbacks = [checkpoint], batch_size = 32)
-
+# model.fit(X_train, y_train, epochs = 25, validation_split = 0.1, callbacks = [checkpoint], batch_size = 32)
+model.fit_generator(datagen.flow(X_train, y_train, batch_size = 32), epochs = 25, steps_per_epoch = X_train.shape[0] / 32, validation_data = (X_val, y_val), callbacks = [checkpoint])
 
 loss, acc = model.evaluate(X_test, y_test)
 
 print('loss: {}, acc: {}'.format(loss, acc))
 
 
-#mnist dataset: loss: 0.08611345658177452, acc: 0.9904
-#cifar10 dataset: loss: 1.3713039035797119, acc: 0.6387
+#loss: 1.3713039035797119, acc: 0.6387
+
+#loss: 1.1491667903900147, acc: 0.6625
 
